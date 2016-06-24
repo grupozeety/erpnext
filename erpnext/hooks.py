5a0b43d9be11627137765e1c52db64d289b2999d
paulocoronado <paulocoronado@gmail.com>
2016-06-24 14:36:11 -0500

    hooks bug

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -41,6 +41,8 @@ email_append_to = ["Job Applicant", "Opportunity", "Issue"]
 calendars = ["Task", "Production Order", "Time Log", "Leave Application", "Sales Order", "Holiday List"]
 
 fixtures = ["Web Form"]
+fixtures = ["Custom Field"]
+fixtures = ["Custom Script"]
 
 website_generators = ["Item Group", "Item", "Sales Partner", "Job Opening"]
 
